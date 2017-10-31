472cd700295729e01ca50dc5f28204dbc776f2f6
Daniel Leal <danielleallopez@gmail.com>
2017-10-31 19:20:50 +0100

    Implement Splash screen UI - no logic

---
+++ app/src/main/java/com/dleal/moviedb/ui/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.dleal.moviedb.ui.splash
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.dleal.moviedb.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
